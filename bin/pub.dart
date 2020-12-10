0d358f7ff40f6b10cee6beff051b2d23d340a1cd
zhuzhe <mason@sfmail.sf-express.com>
2020-12-10 12:05:29 +0800

    add custom default server in comment

--- bin/pub.dart
+++ bin/pub.dart
@@ -4,6 +4,16 @@
 
 import 'package:pub/src/command_runner.dart';
 
+///
+/// use below command to generate a snapshot 
+/// dart --snapshot=xxx.snapshot ./bin/pub.dart
+///
 void main(List<String> arguments) {
+  // if (arguments == null || arguments.isEmpty) {
+  //   arguments = [];
+  //   arguments.add("publish");
+  //   arguments.add("--server");
+  //   arguments.add("http://some.private.host");
+  // }
   PubCommandRunner().run(arguments);
 }
